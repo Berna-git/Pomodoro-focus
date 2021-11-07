ff60878135dcad0680e465bce132b21126c76d6e
Bernabee Gonzalez <83044002+Berna-git@users.noreply.github.com>
2021-11-06 21:16:34 -0300

    subo new changes

--- index.js
+++ index.js
@@ -1,40 +1,64 @@
-// setInterval(() => {
-//     let dateNow = new Date();
-//     console.log(dateNow)
-// }, 1000);
+//Get buttons
+let focusPomodoro =  document.getElementById('time-Pomodoro');
+let timeShortBreak = document.getElementById('time-short_break');
+let timeLongBreak = document.getElementById('time-long_break');
+let watchTimer = document.getElementById('div-watch-timer');
+//Get button of start timer
+let btnTimer = document.getElementById('btn-start_time');   
+// Time Of Timer
+let timeWork = 25;
+let timeShort = 5;
+let timeLong = 15;
+let seconds = 60;
+watchTimer.value = 1;
+ 
+// Add listener
+focusPomodoro.addEventListener('click',function(){
+    watchTimer.value = 1;
+    watchTimer.innerHTML = `<p>${timeWork}:00<p>`
+})
 
-// setInterval(function(){ alert("Hello"); }, 3000);
+timeShortBreak.addEventListener('click',timeShortBreking);
 
-// setTimeout(()=>{
-//      alert("Hello"); 
-//     }, 3000);
+timeLongBreak.addEventListener('click',function(){
+    watchTimer.value = 3;
+    watchTimer.innerHTML = `<p>${timeLong}:00`
+})
 
-const getTimeNow = () => {
-    let timeUse = new Date();
-    // timeUse = timeUse - 1000;
-    let timePomodorMinutes = timeUse.getMinutes();
-    let timePomodorSeconds = timeUse.getSeconds();
-    // console.log(timeUse);
-    console.log(timeUse);
-    let timeNow = `${timePomodorMinutes} : ${timePomodorSeconds} `;
-    return timeNow;
+function timeShortBreking(){
+    watchTimer.value = 2;
+    watchTimer.innerHTML = `<p>0${timeShort}:00<p>`
 }
 
+/// Start timer
+// btnTimer.addEventListener('click',function(){
+//     console.log(watchTimer.value)
+// })
+btnTimer.addEventListener('click',startTimer);
+console.log(watchTimer.value)
+function startTimer() {
+    setInterval(() => {
+        if(watchTimer.value == 1) {
+            if (seconds > 0){
+                seconds = seconds - 1;
+                console.log(`${timeWork}:${seconds}`) 
+                return seconds;  
+            } else if(timeWork > 0) {
+                seconds = 60;
+                timeWork = timeWork - 1;
+                console.log(`${timeWork}:${seconds}`)            
+                return seconds, timeWork;
+            } else {
+                timeWork = 25;
+                timeShortBreking();
+                btnTimer.removeAttribute('btn-start_time');
+            }
 
-// setInterval(() => {
-//     // let timePomodoro = getTimeNow()
-//     console.log(getTimeNow()); 
-// }, 1000);
-
-
-// for(let i = 60; i > 0; i = i - 1){
-//     setTimeout(() => {
-//         console.log(i);
-//     }, 1000);          
-// }
-
-let i = 60;
-while(i > 0) {
-        console.log(i)        
-    i--;
+        }     
+    }, 1);
+    
 }
+// console.log(seconds)
+// document.getElementById("myBtn").addEventListener("click", function() {
+//     document.getElementById("demo").innerHTML = "Hello World";
+//   });
