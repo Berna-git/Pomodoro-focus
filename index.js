3e1490d14d9da9aafacfc33958e55b40d888d2e4
Bernabee Gonzalez <83044002+Berna-git@users.noreply.github.com>
2021-11-08 19:46:17 -0300

    Pomodoro realizado

--- index.js
+++ index.js
@@ -1,60 +1,138 @@
 //Get buttons
-let focusPomodoro =  document.getElementById('time-Pomodoro');
-let timeShortBreak = document.getElementById('time-short_break');
-let timeLongBreak = document.getElementById('time-long_break');
-let watchTimer = document.getElementById('div-watch-timer');
-//Get button of start timer
-let btnTimer = document.getElementById('btn-start_time');   
+const focusPomodoro =  document.getElementById('time-Pomodoro');
+const timeShortBreak = document.getElementById('time-short_break');
+const timeLongBreak = document.getElementById('time-long_break');
+const watchTimer = document.getElementById('div-watch-timer');
+//Get buttons of start timer and pause and clear
+const btnTimer = document.getElementById('btn-start_time'); 
+const btnClear = document.getElementById('btn-reset')
 // Time Of Timer
 let timeWork = 25;
 let timeShort = 5;
 let timeLong = 15;
-let seconds = 60;
+let seconds = 0;
+let startTimer;
 watchTimer.value = 1;
  
-// Add listener
+// Add listener to times
 focusPomodoro.addEventListener('click',function(){
     watchTimer.value = 1;
     watchTimer.innerHTML = `<p>${timeWork}:00<p>`
+    clearTimers()
 })
 
-timeShortBreak.addEventListener('click',timeShortBreking);
+timeShortBreak.addEventListener('click',function(){
+    watchTimer.value = 2;
+    watchTimer.innerHTML = `<p>0${timeShort}:00<p>`
+    clearTimers()
+});
 
 timeLongBreak.addEventListener('click',function(){
     watchTimer.value = 3;
     watchTimer.innerHTML = `<p>${timeLong}:00`
+    clearTimers()
 })
 
-function timeShortBreking(){
-    watchTimer.value = 2;
-    watchTimer.innerHTML = `<p>0${timeShort}:00<p>`
+
+
+btnTimer.addEventListener('click',function(){
+    if(startTimer === undefined){
+        startTimer = setInterval(timer,1000)
+    }
+});
+
+btnClear.addEventListener('click',clearTimers)
+
+
+function clearTimers(){
+    timeWork = 25;
+    timeShort = 5;
+    timeLong = 15;
+    seconds = 0;
+    if(watchTimer.value == 1) {
+        watchTimer.innerHTML = `<p>${timeWork}:00<p>`
+    } else if (watchTimer.value == 2) {
+        watchTimer.innerHTML = `<p>0${timeShort}:00<p>`
+    } else {
+        watchTimer.innerHTML = `<p>${timeLong}:00`
+    }
+    clearInterval(startTimer)
+    startTimer = undefined;
 }
 
-/// Start timer
-// btnTimer.addEventListener('click',function(){
-//     console.log(watchTimer.value)
-// })
-btnTimer.addEventListener('click',startTimer);
-console.log(watchTimer.value)
-function startTimer() {
-    setInterval(() => {
-        if(watchTimer.value == 1) {
-            if (seconds > 0){
-                seconds = seconds - 1;
-                console.log(`${timeWork}:${seconds}`) 
-                return seconds;  
-            } else if(timeWork > 0) {
-                seconds = 60;
-                timeWork = timeWork - 1;
-                console.log(`${timeWork}:${seconds}`)            
-                return seconds, timeWork;
-            } else {
-                timeWork = 25;
-                timeShortBreking();
-                btnTimer.removeAttribute('btn-start_time');
-            }
+function timer() {
+    if(watchTimer.value == 1) {
+        timerWork()
+    } else if (watchTimer.value == 2) {
+        timerShortBreak();
+    } else {
+        timerLongBreak();
+    }
+}
 
-        }     
-    }, 1);
-    
+function timerWork () {
+        if (seconds > 0){
+           seconds = seconds - 1;
+            addZero(seconds,timeWork)
+           return seconds;  
+       } else if(timeWork > 0) {
+           seconds = 60;
+           timeWork = timeWork - 1;
+           watchTimer.innerHTML = `<p>${timeWork}:${seconds}</p>`;            
+           return seconds, timeWork;
+       } else {
+           timeWork = 25;
+           clearInterval(startTimer)
+           watchTimer.innerHTML = `<p>${timeWork}:${seconds}0</p>`;            
+           startTimer = undefined
+       } 
+}
+
+function timerShortBreak(){
+    if (seconds > 0){
+        seconds = seconds - 1;
+            addZero(seconds,timeShort)
+        return seconds;  
+    }else if(timeShort > 0) {
+        seconds = 60;
+        timeShort = timeShort - 1;
+        watchTimer.innerHTML = `<p>${timeShort}:${seconds}</p>`;            
+        return seconds, timeShort;   
+    } else {
+        timeShort = 5;
+        clearInterval(startTimer)
+        watchTimer.innerHTML = `<p>0${timeShort}:${seconds}0</p>`;            
+        startTimer = undefined
+            
+    }   
+}
+
+function timerLongBreak(){
+    if (seconds > 0){
+        seconds = seconds - 1;
+        addZero(seconds,timeLong);         
+        return seconds;  
+    } else if(timeLong > 0) {
+        seconds = 60;
+        timeLong = timeLong - 1;
+            
+        return seconds, timeLong;
+    } else {
+        timeLong = 15;
+        clearInterval(startTimer)
+        watchTimer.innerHTML = `<p>${timeLong}:${seconds}0</p>`;            
+        startTimer = undefined
+    }   
+}
+
+function addZero(sec,mainTime,){
+    if(sec < 10 && mainTime < 10) {
+        watchTimer.innerHTML = `<p>0${mainTime}:0${seconds}</p>`
+    } else if(sec < 10) {
+        watchTimer.innerHTML = `<p>${mainTime}:0${seconds}</p>`
+    } else if(mainTime < 10) {
+        watchTimer.innerHTML = `<p>0${mainTime}:${seconds}</p>`
+    } else {
+        watchTimer.innerHTML = `<p>${mainTime}:${seconds}</p>`
+    } 
 }
